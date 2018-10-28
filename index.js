fc27eccb5bd4ae1169d03aa8f094f555a86b92f3
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-10-28 14:14:55 +0200

    Probably done

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const { dialogflow, MediaObject, Image } = require("actions-on-google");
 const functions = require("firebase-functions");
 
+const repeatLengthSec = 120;
+
 // --- Sounds -----------------------------------------------------------------------------------------------------
+// https://developers.google.com/actions/tools/sound-library/horror
 const horrorSounds = {
   "1": {
     url: "https://actions.google.com/sounds/v1/horror/aggressive_zombie_snarls.ogg",
@@ -13,159 +16,90 @@ const horrorSounds = {
     url: "https://actions.google.com/sounds/v1/horror/monster_alien_growl_pained.ogg",
     title: "Monster Alien Growl Pained",
     lengthSec: 27
-  }
-};
-
-const kidsSounds = {
-  "1": {
+  },
+  "3": {
+    url: "https://actions.google.com/sounds/v1/human_voices/male_burps.ogg",
+    title: "Male Burps",
+    lengthSec: 8
+  },
+  "4": {
+    url: "https://actions.google.com/sounds/v1/human_voices/man_laugh_and_knee_slap.ogg",
+    title: "Man Laugh And Knee Slap",
+    lengthSec: 33
+  }, 
+  "5": {
+    url: "https://actions.google.com/sounds/v1/horror/female_monster_attacking.ogg",
+    title: "Female Monster Attacking",
+    lengthSec: 8
+  }, 
+  "6": {
+    url: "https://actions.google.com/sounds/v1/horror/female_monster_growls.ogg",
+    title: "Female Monster Growls",
+    lengthSec: 9
+  },
+  "7": {
+    url: "https://actions.google.com/sounds/v1/horror/monster_alien_growl_calm.ogg",
+    title: "Monster Alien Growl Calm",
+    lengthSec: 8
+  },
+  "8": {
+    url: "https://actions.google.com/sounds/v1/horror/monster_alien_growl_pained.ogg",
+    title: "Monster Alien Growl Pained",
+    lengthSec: 27
+  },
+  "9": {
+    url: "https://actions.google.com/sounds/v1/horror/monster_alien_growl_tense.ogg",
+    title: "Monster Alien Growl Tense",
+    lengthSec: 12
+  },
+  "10": {
     url: "https://actions.google.com/sounds/v1/human_sounds/baby_cry_long.ogg",
     title: "Baby Cry Long",
     lengthSec: 41
-  }
+  },
+  "11": {
+    url: "https://actions.google.com/sounds/v1/human_voices/babies_cry.ogg",
+    title: "Babies Cry",
+    lengthSec: 34
+  },  
 };
 
+
 // --- Functions -----------------------------------------------------------------------------------------------------
-// Takes an object with qoutes data and optionally a filter (keys for the quotes)
-// Returns a ssml-formatted string with data of random quote selected from the filtered
-function randomQuote(quotesVariants, myFilter = []) {
-  // We have >1 quotes
-  if (Object.keys(quotesVariants).length > 1) {
-    // We don't have a filter
-    if (myFilter.length === 0) {
-      const randPair = randomKeyPair(quotesVariants);
-      return `<speak><audio src="https://iuriid.github.io/public/img/BestMovieQuotesWAVs/${randPair.url}">}</audio></speak>`;
-    } else {
-      // We have a filter
-      const filteredQuotes = {};
-      myFilter.forEach(key => {
-        filteredQuotes[key] = quotes[key];
-      });
-      const randPair = randomKeyPair(filteredQuotes);
-      return `<speak><audio src="https://iuriid.github.io/public/img/BestMovieQuotesWAVs/${
-        randPair.file
-      }">"${randPair.phrase}" - ${randPair.character} (${randPair.actor}) - ${
-        randPair.movie
-      }</audio><break time="3s"/><audio src="https://iuriid.github.io/public/img/BestMovieQuotesWAVs/talk_to_me_goose.mp3">/ "Talk to me, Goose" - PETE 'MAVERICK' MITCHELL (Tom Cruise) - TOP GUN, Paramount, 1986</audio></speak>`;
-    }
-  }
-
-  // Quotes object has the only quote
-  const theOnlyQuote = quotesVariants[Object.keys(quotesVariants)[0]];
-  return `<speak><audio src="https://iuriid.github.io/public/img/BestMovieQuotesWAVs/${
-    theOnlyQuote.file
-  }">"${theOnlyQuote.phrase}" - ${theOnlyQuote.character} (${
-    theOnlyQuote.actor
-  }) - ${
-    theOnlyQuote.movie
-  }</audio><break time="3s"/><audio src="https://iuriid.github.io/public/img/BestMovieQuotesWAVs/talk_to_me_goose.mp3"> / "Talk to me, Goose" - PETE 'MAVERICK' MITCHELL (Tom Cruise) - TOP GUN, Paramount, 1986</audio></speak>`;
+function randomSound(soundLib) {
+  const soundLibLength = Object.keys(soundLib).length;
+  const randKey = Math.floor(Math.random() * soundLibLength) + 1;
+  return randKey.toString();
 }
 
-// Helper function - returns a random key-value pair from a given object
-function randomKeyPair(quotesVariants) {
-  const quotesKeys = Object.keys(quotesVariants);
-  const randKey = Math.round(Math.random() * (quotesKeys.length - 1));
-  return quotesVariants[quotesKeys[randKey]];
-}
-
-// Instantiate the Dialogflow client with debug logging enabled.
+// --- Instantiate the Dialogflow client with debug logging enabled --------------------------------------------------
 const app = dialogflow({
   debug: true
 });
 
-// --- Dialogs -----------------------------------------------------------------------------------------------------
-app.intent("random.quote", conv => {
-  const ssml = randomQuote(quotes);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.hello", conv => {
-  const relevantQuotesKeys = ["10", "80", "81", "8", "20", "49", "52", "61", "63", "66", "68"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.whatsup", conv => {
-  const relevantQuotesKeys = ["1", "44", "50"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.whatcanyoudo", conv => {
-  const relevantQuotesKeys = ["2", "30"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.whereareyou", conv => {
-  const relevantQuotesKeys = ["4", "41", "50"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
+// --- Dialogs -------------------------------------------------------------------------------------------------------
+app.intent("creepysounds.input.welcome", conv => {
+  const horrorSoundKey = randomSound(horrorSounds);
+  const kidsSoundKey = randomSound(kidsSounds);
+  const ssml = `
+  <speak>
+    <audio src="${horrorSounds[horrorSoundKey].url}" soundLevel="+30db">
+      ${horrorSounds[horrorSoundKey].title}
+    </audio>
+  </speak>`
   console.log(`SSML: ${ssml}`);
   conv.ask(ssml);
 });
 
 app.intent("default.fallback", conv => {
-  const relevantQuotesKeys = ["8", "11", "34", "50", "55", "88"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.goodmorning", conv => {
-  const relevantQuotesKeys = ["12"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.whatislove", conv => {
-  const relevantQuotesKeys = ["13"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.whatisit", conv => {
-  const relevantQuotesKeys = ["14"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.agent.acquaintance", conv => {  
-  const relevantQuotesKeys = ["22", "63", "65", "68", "75", "100"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.how_are_you", conv => {
-  const relevantQuotesKeys = ["19", "30", "38", "40", "41", "44", "53"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.greetings.bye", conv => {
-  const relevantQuotesKeys = ["37", "47", "61", "76"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.agent.my_friend", conv => {
-  const relevantQuotesKeys = ["56", "58", "63"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
-  console.log(`SSML: ${ssml}`);
-  conv.ask(ssml);
-});
-
-app.intent("smalltalk.whatislife", conv => {
-  const relevantQuotesKeys = ["93"];
-  const ssml = randomQuote(quotes, relevantQuotesKeys);
+  const horrorSoundKey = randomSound(horrorSounds);
+  const kidsSoundKey = randomSound(kidsSounds);
+  const ssml = `
+  <speak>
+    <audio src="${horrorSounds[horrorSoundKey].url}" soundLevel="+30db">
+      ${horrorSounds[horrorSoundKey].title}
+    </audio>
+  </speak>`
   console.log(`SSML: ${ssml}`);
   conv.ask(ssml);
 });
